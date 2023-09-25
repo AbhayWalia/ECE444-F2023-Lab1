c06af455efe5988955306d9f1f1450c857a7feeb
Abhay Walia <abhaywalia2002@gmail.com>
2023-09-24 21:11:08 -0400

    Created web page in example4.py with all the functionality as outlined in the instructions

--- example4.py
+++ example4.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, validators
+from wtforms.validators import DataRequired, Email, ValidationError
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,7 +16,11 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    # email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired()])
+
     submit = SubmitField('Submit')
+
 
 
 @app.errorhandler(404)
@@ -32,8 +38,23 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+        
+        if '@' not in form.email.data:
+           flash("Please include an '@' in the email address. " + form.email.data + " is missing a '@'")
+           return redirect(url_for('index'))
+           
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+       
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+    
+
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    # return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name =session.get('name'), email = session.get('email'))
+
